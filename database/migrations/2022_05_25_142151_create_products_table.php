36bf194e62334cb280019e8547b0c399c33ad412
Habib Ali Habsyi <habibalihabsyi123@gmail.com>
2022-05-25 21:40:12 +0700

    product database & model

---
+++ database/migrations/2022_05_25_142151_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->integer('admin_id');
+            $table->string('name');
+            $table->string('sku');
+            $table->integer('price');
+            $table->string('image')->nullable();
+            $table->string('link')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
