2a01edf63df4fa3ced253c600a0dae59b3233d46
Hafidz <17102057@ittelkom-pwt.ac.id>
2022-06-10 08:43:37 +0700

    Change Model

--- app/Models/FacebookEventPixel.php
+++ app/Models/FacebookEventPixel.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class FacebookEventPixel extends Model
 {
     use HasFactory;
-    protected $guarded = ['*'];
+    protected $table = 'facebook_event_pixels';
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,7 +9,10 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Product extends Model
 {
     use HasFactory;
-    use SoftDeletes;
+    //use SoftDeletes;
 
-    protected $guarded = ['id'];
+    //protected $guarded = ['id'];
+
+    protected $table = 'products';
+    
 }
