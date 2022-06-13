59895edc85ba35032aa9aa5baae2772829fbd0cf
VEZA ADILOVIANA <18102143@ittelkom-pwt.ac.id>
2022-06-13 08:56:03 +0700

    add navbar for page cs

---
+++ app/Http/Livewire/Navbar/NavbarCs2.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Navbar;
+
+use Livewire\Component;
+
+class NavbarCs2 extends Component
+{
+    public function render()
+    {
+        return view('livewire.navbar.navbar-cs2');
+    }
+}
