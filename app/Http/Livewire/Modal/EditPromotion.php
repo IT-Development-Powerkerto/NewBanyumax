3fc26eab60d0086e5ee73986956432ede5a0dc58
VEZA ADILOVIANA <18102143@ittelkom-pwt.ac.id>
2022-06-14 15:35:27 +0700

    modal add promotion, edit promotion, delete

---
+++ app/Http/Livewire/Modal/EditPromotion.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Modal;
+
+use Livewire\Component;
+
+class EditPromotion extends Component
+{
+    public function render()
+    {
+        return view('livewire.modal.edit-promotion');
+    }
+}
