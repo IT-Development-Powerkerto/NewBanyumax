2acaaece549939938e6d1660e23e71a1a9b96d98
VEZA ADILOVIANA <18102143@ittelkom-pwt.ac.id>
2022-06-14 10:06:27 +0700

    modal add product

---
+++ app/Http/Livewire/Modal/AddProduct2.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Modal;
+
+use Livewire\Component;
+
+class AddProduct2 extends Component
+{
+    public function render()
+    {
+        return view('livewire.modal.add-product2');
+    }
+}
