312206863286f34be23a95b578b028bb326185a5
VEZA ADILOVIANA <18102143@ittelkom-pwt.ac.id>
2022-06-17 13:32:16 +0700

    add overflow-y-auto in operator

--- resources/views/livewire/table/operator-list.blade.php
+++ resources/views/livewire/table/operator-list.blade.php
@@ -1,9 +1,111 @@
-<div class="p-4 mt-7 max-h bg-white rounded-lg border shadow-md dark:bg-gray-800 dark:border-gray-700">
+<div class="p-4 mt-12 h-96 overflow-y-auto bg-white rounded-lg border shadow-md dark:bg-gray-800 dark:border-gray-700">
     <div class="flex justify-between items-center mb-4">
         <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white ">Operator List</h5>
     </div>
     <div class="flow-root overflow-x-auto">
         <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+            <li class="flex flex-coloumn py-3 sm:py-4">
+                <div class="flex items-center space-x-4">
+                    <div class="flex-shrink-0">
+                        <img class="w-8 h-max rounded-full" src="assets/img/favicon.png"
+                            alt="Neil image">
+                    </div>
+                    <div class="flex-1 min-w-0">
+                        <p class="text-sm font-semibold text-black truncate dark:text-white">
+                            Mei Dwila Nawa Sapta Ningtyas
+                        </p>
+                        <p class="text-sm font-reguler text-gray-500 truncate dark:text-white">
+                            +1234567890
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-white">jordan@mail.com</p>
+                    </div>
+                </div>
+            </li>
+            <li class="flex flex-coloumn py-3 sm:py-4">
+                <div class="flex items-center space-x-4">
+                    <div class="flex-shrink-0">
+                        <img class="w-8 h-max rounded-full" src="assets/img/favicon.png"
+                            alt="Neil image">
+                    </div>
+                    <div class="flex-1 min-w-0">
+                        <p class="text-sm font-semibold text-black truncate dark:text-white">
+                            Mei Dwila Nawa Sapta Ningtyas
+                        </p>
+                        <p class="text-sm font-reguler text-gray-500 truncate dark:text-white">
+                            +1234567890
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-white">jordan@mail.com</p>
+                    </div>
+                </div>
+            </li>
+            <li class="flex flex-coloumn py-3 sm:py-4">
+                <div class="flex items-center space-x-4">
+                    <div class="flex-shrink-0">
+                        <img class="w-8 h-max rounded-full" src="assets/img/favicon.png"
+                            alt="Neil image">
+                    </div>
+                    <div class="flex-1 min-w-0">
+                        <p class="text-sm font-semibold text-black truncate dark:text-white">
+                            Mei Dwila Nawa Sapta Ningtyas
+                        </p>
+                        <p class="text-sm font-reguler text-gray-500 truncate dark:text-white">
+                            +1234567890
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-white">jordan@mail.com</p>
+                    </div>
+                </div>
+            </li>
+            <li class="flex flex-coloumn py-3 sm:py-4">
+                <div class="flex items-center space-x-4">
+                    <div class="flex-shrink-0">
+                        <img class="w-8 h-max rounded-full" src="assets/img/favicon.png"
+                            alt="Neil image">
+                    </div>
+                    <div class="flex-1 min-w-0">
+                        <p class="text-sm font-semibold text-black truncate dark:text-white">
+                            Mei Dwila Nawa Sapta Ningtyas
+                        </p>
+                        <p class="text-sm font-reguler text-gray-500 truncate dark:text-white">
+                            +1234567890
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-white">jordan@mail.com</p>
+                    </div>
+                </div>
+            </li>
+            <li class="flex flex-coloumn py-3 sm:py-4">
+                <div class="flex items-center space-x-4">
+                    <div class="flex-shrink-0">
+                        <img class="w-8 h-max rounded-full" src="assets/img/favicon.png"
+                            alt="Neil image">
+                    </div>
+                    <div class="flex-1 min-w-0">
+                        <p class="text-sm font-semibold text-black truncate dark:text-white">
+                            Mei Dwila Nawa Sapta Ningtyas
+                        </p>
+                        <p class="text-sm font-reguler text-gray-500 truncate dark:text-white">
+                            +1234567890
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-white">jordan@mail.com</p>
+                    </div>
+                </div>
+            </li>
+            <li class="flex flex-coloumn py-3 sm:py-4">
+                <div class="flex items-center space-x-4">
+                    <div class="flex-shrink-0">
+                        <img class="w-8 h-max rounded-full" src="assets/img/favicon.png"
+                            alt="Neil image">
+                    </div>
+                    <div class="flex-1 min-w-0">
+                        <p class="text-sm font-semibold text-black truncate dark:text-white">
+                            Mei Dwila Nawa Sapta Ningtyas
+                        </p>
+                        <p class="text-sm font-reguler text-gray-500 truncate dark:text-white">
+                            +1234567890
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-white">jordan@mail.com</p>
+                    </div>
+                </div>
+            </li>
             <li class="flex flex-coloumn py-3 sm:py-4">
                 <div class="flex items-center space-x-4">
                     <div class="flex-shrink-0">
