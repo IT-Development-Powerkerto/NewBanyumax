bfd9aa3fe1e60f8a5ff9be4e5bd5a276c092f0b4
HafidzDaffa <81839686+HafidzDaffa@users.noreply.github.com>
2022-06-20 10:06:48 +0700

    login

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -34,51 +35,52 @@ class LoginController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->input();
+        if(Auth::attempt(['username' => $data['username'], 'password' => $data['password']])) {
+            $role_id = auth()->user()->role_id;
+            $admin_id = auth()->user()->admin_id;
+            // $exp = auth()->user()->exp;
+
+            // if ($exp == 0){
+            //     $request->session()->invalidate();
+            //     $request->session()->regenerateToken();
+            //     return back()->with('error', 'sorry your account is being processed, we will notify you via email');
+            // }
+            // elseif($admin_id == 1){
+            //     $request->session()->regenerate();
+            //     return redirect()->intended('/superadmin');
+            // }
+            // elseif($role_id == 1){
+            //     $request->session()->regenerate();
+            //     return redirect()->intended('/dashboard');
+            // }
+            if($role_id == 4){
+                $request->session()->regenerate();
+                return redirect()->intended('/dashboard-adv');
+            }
+            elseif($role_id == 5 || $role_id == 13){
+                $request->session()->regenerate();
+                return redirect()->intended('/dashboard-cs');
+            }
+        }
+        // elseif (Auth::attempt(['username' => $data['username'], 'password' => $data['password'], 'role_id' => '4'])){
+        //     $request->session()->regenerate();
+        //     return redirect()->intended('adv');
+        // }
+        // elseif (Auth::attempt(['username' => $data['username'], 'password' => $data['password'], 'role_id' => '5'])){
+        //     $request->session()->regenerate();
+        //     return redirect()->intended('cs');
+        // }
+        return back()->with('error', 'Login Failed!');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
+    public function logout(Request $request){
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
