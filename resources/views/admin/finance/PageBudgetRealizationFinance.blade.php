999ef18cfb5995c92a6c344f4f815ce92a3e3dde
VEZA ADILOVIANA <18102143@ittelkom-pwt.ac.id>
2022-06-23 09:12:55 +0700

    add page, content, modal, bdgt real role fin admin

---
+++ resources/views/admin/finance/PageBudgetRealizationFinance.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="assets/img/favicon.png" rel="icon">
+    <title>Finance</title>
+    <link href="../css/app.css" rel="stylesheet">
+
+    <link rel="stylesheet"
+        href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+    @livewireStyles
+</head>
+
+<body class="bg-zinc-200" style="font-family: 'Poppins', sans-serif;">
+    <livewire:navbar.navbar-admin />
+    <div class="mx-5">
+        <livewire:table.budgetingrealization-finance-admin />
+        <livewire:modal.add-budgeting-realization-finance-admin />
+    </div>
+
+    @livewireScripts
+    <script src="assets/vendor/flowbite/dist/flowbite.js"></script>
+    <script src="assets/vendor/flowbite/dist/datepicker.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <script>
+        $(function() {
+            $('input[name="selectdate"]').daterangepicker({
+                opens: 'left'
+            }, function(start, end, label) {
+                console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end
+                    .format('YYYY-MM-DD'));
+            });
+        });
+    </script>
+</body>
+
+</html>
