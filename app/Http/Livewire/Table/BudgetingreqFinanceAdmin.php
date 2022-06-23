409361a714e9b7a3064acca0d84ea7cbc4e48e35
VEZA ADILOVIANA <18102143@ittelkom-pwt.ac.id>
2022-06-23 09:12:27 +0700

    add page, content, modal, bdgt req role fin admin

---
+++ app/Http/Livewire/Table/BudgetingreqFinanceAdmin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Table;
+
+use Livewire\Component;
+
+class BudgetingreqFinanceAdmin extends Component
+{
+    public function render()
+    {
+        return view('livewire.table.budgetingreq-finance-admin');
+    }
+}
