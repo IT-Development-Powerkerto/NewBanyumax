bd9549ec60ccfec617c6cb7c5e04e2fa45d2e8fe
Muhammad Iqbal Ainu Rafie <74098172+ainurafie@users.noreply.github.com>
2022-06-24 13:42:56 +0700

    Routing

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,20 @@ use App\Http\Controllers\DashboardInputer;
 use App\Http\Controllers\ViewDataClosing;
 use App\Http\Controllers\OperatorController;
 use App\Http\Controllers\EditLeadTunneling;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\DashboardAdmin;
+use App\Http\Controllers\DashboardCeo;
+use App\Http\Controllers\DashboardManager;
+use App\Http\Controllers\BudgetingRequestInputer;
+use App\Http\Controllers\BudgetingRealizationInputer;
+use App\Http\Controllers\CampaignAdmin;
+use App\Http\Controllers\OperatorAdmin;
+use App\Http\Controllers\BudgetingAdvertisingAdmin;
+use App\Http\Controllers\BudgetingRealizationAdmin;
+use App\Http\Controllers\RountineEvaluationAdmin;
+use App\Http\Controllers\CreatePromotionCustomerService;
+use App\Http\Controllers\ReimbursmentCustomerService;
+use App\Http\Controllers\BudgetingRealizationCustomerService;
+use App\Http\Controllers\RoutineEvaluationCustomerService;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,24 +49,53 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Login Logout
 Route::resource('/login', LoginController::class);
-Route::get('/logout',[LoginController::class, 'logout']);
 
 Route::resource('/editleadtunneling', EditLeadTunneling::class);
 // Users
 
 Route::resource( '/realization', BudgetingRealizationController::class);
-
+// Admin.Dashboard
+Route::resource('/dashboard-admin', DashboardAdmin::class);
+// Admin.CEO.Dashboard
+Route::resource('/dashboard-ceo', DashboardCeo::class);
+// Admin.Manager.Admin
+Route::resource('/dashboard-manager', DashboardManager::class);
+// Admin.cs
+Route::resource('/createpromotion-cs-admin', CreatePromotionCustomerService::class);
+Route::resource('/reimbursment-cs-admin', ReimbursmentCustomerService::class);
+Route::resource('/budgetingrealization-cs-admin', BudgetingRealizationCustomerService::class);
+Route::resource('/routineevaluation-cs-admin', RoutineEvaluationCustomerService::class);
+// Admin.Inputer
+Route::resource('/dashboard-inputer', DashboardInputer::class);
+Route::resource('/budgetingrequest-inputer', BudgetingRequestInputer::class);
+Route::resource('/budgetingrealization-inputer', BudgetingRealizationInputer::class);
+// Admin.Adv
+Route::resource('/campaign-admin', CampaignAdmin::class);
+Route::resource('/operator-admin', OperatorAdmin::class);
+Route::resource('/budgetingadvertising-admin', BudgetingAdvertisingAdmin::class);
+Route::resource('/budgetingrealization-admin', BudgetingRealizationAdmin::class);
+Route::resource('/rountineevaluation-admin', RountineEvaluationAdmin::class);
+// Admin. Finance
+Route::get('/dashboard-finance', function () {
+    return view('admin.finance.PageDashboard');
+});
+Route::get('/dashboard-budgetrequest-finance', function () {
+    return view('admin.finance.PageBudgetRequestFinance');
+});
+Route::get('/dashboard-budgetrealization-finance', function () {
+    return view('admin.finance.PageBudgetRealizationFinance');
+});
 
 // Users.Campaign
-Route::resource('/campaign', CampaignController::class);
-
-
-// Users.Product
+// Route::resource('/campaign', CampaignController::class);
+Route::get('/campaign-adv', [CampaignController::class, 'index']) ->name('campaign.index');
+Route::get('/campaign-adv/create', [CampaignController::class, 'create']) ->name('campaign.create');
+Route::post('/campaign-adv/store', [CampaignController::class, 'store']) ->name('campaign.store');
+Route::get('/campaign-adv/edit/{id}', [CampaignController::class, 'edit']) ->name('campaign.edit');
+Route::patch('/campaign-adv/update/{id}', [CampaignController::class, 'update']) ->name('campaign.update');
+Route::get('/campaign-adv/delete/{id}', [CampaignController::class, 'destroy']) ->name('campaign.destroy');
 Route::resource('/product', ProductController::class);
-
-
 // Users.Advertiser
 Route::resource('/dashboard-adv', DashboardController::class);
 Route::resource('/operator-adv', OperatorController::class);
@@ -84,5 +126,4 @@ Route::resource('/finance', FinanceDashboard::class);
 Route::resource('/budgetingrequest', BudgetingRequest::class);
 
 //User.Inputer
-Route::resource('/inputer', DashboardInputer::class);
 Route::resource('/viewdataclosing', ViewDataClosing::class);
