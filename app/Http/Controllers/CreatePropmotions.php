222398dc97a9c01d325cc6dc298ae1ecbc53b427
HafidzDaffa <81839686+HafidzDaffa@users.noreply.github.com>
2022-06-28 15:54:19 +0700

    Change controller promotion

--- app/Http/Controllers/CreatePropmotions.php
+++ app/Http/Controllers/CreatePropmotions.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Promotion;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class CreatePropmotions extends Controller
 {
@@ -13,7 +17,12 @@ class CreatePropmotions extends Controller
      */
     public function index()
     {
-        return view('User.Cs.PagePromotion');
+        $product = Product::where('admin_id', auth()->user()->admin_id)->get();
+        // $promotion = Promotion::where('admin_id', auth()->user()->admin_id)->get();
+        $promotion_cs = Promotion::where('admin_id', auth()->user()->admin_id)->where('user_id', auth()->user()->id)->get();
+        if(auth()->user()->role_id==5 || auth()->user()->role_id==13){
+            return view('User.Cs.PagePromotion')->with('product', $product)->with('promotion', $promotion_cs);
+        }
     }
 
     /**
@@ -34,7 +43,21 @@ class CreatePropmotions extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $total_promotion = $request->promotion_product_price;
+
+        DB::table('promotions')->insert([
+            'admin_id'                      => auth()->user()->admin_id,
+            'user_id'                       => auth()->user()->id,
+            'promotion_name'                => $request->promotion_name,
+            'product_id'                    => $request->product_id,
+            'promotion_product_price'       => $request->promotion_product_price,
+            'promotion_product_percent'     => $request->promotion_product_percent,
+            'total_promotion'               => $total_promotion,
+            'created_at'                    => Carbon::now()->toDateTimeString(),
+            'updated_at'                    => Carbon::now()->toDateTimeString(),
+        ]);
+
+        return back();
     }
 
     /**
@@ -56,7 +79,11 @@ class CreatePropmotions extends Controller
      */
     public function edit($id)
     {
-        //
+        $product = Prfoduct::where('admin_id', auth()->user()->admin_id)->get();
+        $promotion = Promotion::where('admin_id', auth()->user()->admin_id)->whereId($id)->get();
+        if(auth()->user()->role_id == 5 || auth()->user()->role_id == 13){
+            return view('livewire.modal.edit-promotion', ['promotion' => $promotion])->with('product', $product);
+        }
     }
 
     /**
@@ -68,7 +95,20 @@ class CreatePropmotions extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $total_promotion = $request->promotion_product_price;
+
+        DB::table('promotions')->where('id',$id)->update([
+            'admin_id'                      => auth()->user()->admin_id,
+            'user_id'                       => auth()->user()->id,
+            'promotion_name'                => $request->promotion_name,
+            'product_id'                    => $request->product_id,
+            'promotion_product_price'       => $request->promotion_product_price,
+            'promotion_product_percent'     => $request->promotion_product_percent,
+            'total_promotion'               => $total_promotion,
+            'created_at'                    => Carbon::now()->toDateTimeString(),
+            'updated_at'                    => Carbon::now()->toDateTimeString(),
+        ]);
+        return back();
     }
 
     /**
@@ -77,8 +117,9 @@ class CreatePropmotions extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Promotion $promotion)
     {
-        //
+        $promotion->delete();
+        return back();
     }
 }
