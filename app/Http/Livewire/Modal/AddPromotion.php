bb852b16166269f83c81f641d0cfd95029b750fb
HafidzDaffa <daffa.hafidz321@gmail.com>
2022-07-04 21:58:10 +0700

    push component add promtion

--- app/Http/Livewire/Modal/AddPromotion.php
+++ app/Http/Livewire/Modal/AddPromotion.php
@@ -8,10 +8,27 @@ use App\Models\Promotion;
 
 class AddPromotion extends Component
 {
+    public $promotion_name, $product_id, $promotion_product_price, $promotion_product_percent, $total_promotion, $admin_id;
+
     public function render()
     {
-        $promotions = Promotion::all();
         $products = Product::all();
-        return view('livewire.modal.add-promotion', compact('products', 'promotions'));
+        return view('livewire.modal.add-promotion', compact('products'));
+    }
+
+    public function store()
+    {
+        // $total_promotion = $this->promotion_product_price;
+        Promotion::create([
+            // 'admin_id'                      => auth()->user()->admin_id,
+            // 'user_id'                       => auth()->user()->id,
+            'promotion_name'                => $this->promotion_name,
+            'product_id'                    => $this->product_id,
+            'promotion_product_price'       => $this->promotion_product_price,
+            'promotion_product_percent'     => $this->promotion_product_percent,
+            // 'total_promotion'               => $total_promotion,
+            'created_at'                    => Carbon::now()->toDateTimeString(),
+            'updated_at'                    => Carbon::now()->toDateTimeString(),
+        ]);
     }
 }
