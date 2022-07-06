72d6903dc009a5b76399cd4b31cb2ae54d4ddd2a
VEZA ADILOVIANA <18102143@ittelkom-pwt.ac.id>
2022-07-06 10:39:19 +0700

    layout chart bar closing rate, card lead

--- resources/views/admin/LiveReporting.blade.php
+++ resources/views/admin/LiveReporting.blade.php
@@ -52,177 +52,61 @@
         </div>
     </div>
 
-    <div class="bg-white mx-5 mb-10">
-        <div class="min-w-44 overflow-x-auto">
-            <ul id="tabs" class="inline-flex w-full border-b text-center">
-                <li
-                    class="flex-grow bg-gray-50 px-8 text-gray-800 font-semibold py-5 border-t border-r-2 border-l-2 -mb-px">
-                    <a id="default-tab" href="#first">Total Omset</a>
-                </li>
-                <li class="flex-grow px-8 text-gray-800 font-semibold py-5"><a href="#second">Total Bottle</a></li>
-                <li class="flex-grow px-8 text-gray-800 font-semibold py-5"><a href="#third">Total Lead</a></li>
-                <li class="flex-grow px-8 text-gray-800 font-semibold py-5"><a href="#fourth">Total Closing</a></li>
-            </ul>
+    <livewire:card.line-chart-reporting />
+    <div class="flex flex-col md:flex-row lg:flex-row gap-4 mx-5 py-5">
+
+        <div class="w-full">
+            <livewire:card.report-closing-rate />
         </div>
 
-        <!-- Tab Contents -->
-        <div id="tab-contents">
-            <div id="first" class="p-4">
-                <canvas class="p-4" id="lineChartTotalOmset"></canvas>
-                {{-- <canvas id="chartLine"> </canvas> --}}
-            </div>
-            <div id="second" class="hidden p-4">
-                <canvas class="p-4" id="lineChartTotalBottle"></canvas>
-            </div>
-            <div id="third" class="hidden p-4">
-                <canvas class="p-4" id="lineChartTotalLead"></canvas>
-            </div>
-            <div id="fourth" class="hidden p-4">
-                <canvas class="p-4" id="lineChartTotalClosing"></canvas>
-            </div>
+        <div class="w-full flex flex-wrap gap-4 justify-between place-content-between">
+            <livewire:card.caq-lead-report />
+            <livewire:card.caq-customer-report />
+            <livewire:card.caq-lead-report />
+            <livewire:card.caq-lead-report />
         </div>
+
     </div>
 
 
     @livewireScripts
+    <!-- Chart bar -->
     <script>
-        //Chart Total Omset
-        var ctxL = document.getElementById("lineChartTotalOmset").getContext('2d');
-        var myLineChart = new Chart(ctxL, {
-          type: 'line',
-          data: {
-            labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Agst", "Sept", "Okt" ,"Nov", "Dec"],
-            datasets: [
-            {
-              label: "Generous",
-              data: [0, 10, 5, 2, 20, 30, 45, 5, 2, 20, 30, 45],
-              backgroundColor: ['rgb(0, 178, 255, .2)'],
-              borderColor: ['#00B2FF'],
-              borderWidth: 2
+        const labelsBarChart = [
+            "Sun",
+            "Mon",
+            "Tues",
+            "Wed",
+            "Thurs",
+            "Fri",
+            "Sat",
+        ];
+        const dataBarChart = {
+            labels: labelsBarChart,
+            datasets: [{
+                label: "",
+                backgroundColor: "hsl(252, 82.9%, 67.8%)",
+                borderColor: "hsl(252, 82.9%, 67.8%)",
+                data: [5, 10, 5, 2, 20, 30, 45, 38],
             }, {
-              label: "Freshmag",
-              data: [10, 20, 5, 15, 25, 34, 18, 38, 43, 27, 31, 14],
-              backgroundColor: ['rgb(59, 130, 246, .2)'],
-              borderColor: ['#3B82F6'],
-              borderWidth: 2
-            },{
-              label: "Gizdat",
-              data: [15,26,40,16,36,49,5,17,23,20,15,20],
-              backgroundColor: ['rgb(0, 187, 85, .2)'],
-              borderColor: ['#00BB55'],
-              borderWidth: 2
+                label: "",
+                backgroundColor: "#d3d6db",
+                borderColor: "#d3d6db",
+                data: [4, 8, 3, 1, 18, 28, 40, 35],
             },
-            ]
-          },
-          options: {
-            responsive: true
-          }
-        });
-    </script>
-
-    <script>
-        //Chart Total Bottle
-        var ctxL = document.getElementById("lineChartTotalBottle").getContext('2d');
-        var myLineChart = new Chart(ctxL, {
-          type: 'line',
-          data: {
-            labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Agst", "Sept", "Okt" ,"Nov", "Dec"],
-            datasets: [
-            {
-              label: "Generous",
-              data: [49,5,17,23,20,15,21,5,26,40,16,36],
-              backgroundColor: ['rgb(0, 178, 255, .2)'],
-              borderColor: ['#00B2FF'],
-              borderWidth: 2
-            }, {
-              label: "Freshmag",
-              data: [ 30, 45, 5, 2, 20, 30, 45,0, 10, 5, 2, 20],
-              backgroundColor: ['rgb(59, 130, 246, .2)'],
-              borderColor: ['#3B82F6'],
-              borderWidth: 2
-            },{
-              label: "Gizdat",
-              data:  [1, 18, 38, 43, 27, 31, 14, 0, 20, 5, 15, 25, 34], 
-              backgroundColor: ['rgb(0, 187, 85, .2)'],
-              borderColor: ['#00BB55'],
-              borderWidth: 2
-            },
-            ]
-          },
-          options: {
-            responsive: true
-          }
-        });
-    </script>
-
-    <script>
-        //Chart Total Lead
-        var ctxL = document.getElementById("lineChartTotalLead").getContext('2d');
-        var myLineChart = new Chart(ctxL, {
-        type: 'line',
-        data: {
-            labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Agst", "Sept", "Okt" ,"Nov", "Dec"],
-            datasets: [
-            {
-            label: "Generous",
-            data: [0, 10,  45, 5, 2, 20, 30, 45, 5, 2, 20, 30],
-            backgroundColor: ['rgb(0, 178, 255, .2)'],
-            borderColor: ['#00B2FF'],
-            borderWidth: 2
-            }, {
-            label: "Freshmag",
-            data: [10, 20, 5, 43, 27, 31, 14, 15, 25, 34, 18, 38],
-            backgroundColor: ['rgb(59, 130, 246, .2)'],
-            borderColor: ['#3B82F6'],
-            borderWidth: 2
-            },{
-            label: "Gizdat",
-            data: [15,26,17,23,20,15,20 ,40,16,36,49,5],
-            backgroundColor: ['rgb(0, 187, 85, .2)'],
-            borderColor: ['#00BB55'],
-            borderWidth: 2
-            },
-            ]
-        },
-        options: {
-            responsive: true
-        }
-        });
-    </script>
-
-    <script>
-        //Chart Total Closing
-        var ctxL = document.getElementById("lineChartTotalClosing").getContext('2d');
-        var myLineChart = new Chart(ctxL, {
-        type: 'line',
-        data: {
-            labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Agst", "Sept", "Okt" ,"Nov", "Dec"],
-            datasets: [
-            {
-            label: "Generous",
-            data: [49,5,17,5,26,40,16,36,23,20,15,21],
-            backgroundColor: ['rgb(0, 178, 255, .2)'],
-            borderColor: ['#00B2FF'],
-            borderWidth: 2
-            }, {
-            label: "Freshmag",
-            data: [ 30, 45, 5, 2, 20, 5, 2, 20, 30, 45,0, 10],
-            backgroundColor: ['rgb(59, 130, 246, .2)'],
-            borderColor: ['#3B82F6'],
-            borderWidth: 2
-            },{
-            label: "Gizdat",
-            data:  [1, 18,  20, 5, 15, 25, 34, 38, 43, 27, 31, 14, 0], 
-            backgroundColor: ['rgb(0, 187, 85, .2)'],
-            borderColor: ['#00BB55'],
-            borderWidth: 2
-            },
-            ]
-        },
-        options: {
-            responsive: true
-        }
-        });
+        ],
+        };
+    
+        const configBarChart = {
+            type: "bar",
+            data: dataBarChart,
+            options: {},
+        };
+    
+        var chartBar = new Chart(
+            document.getElementById("chartBar"),
+            configBarChart
+        );
     </script>
 
     {{-- <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
@@ -260,38 +144,6 @@
             configLineChart
         );
     </script> --}}
-
-    <script>
-        let tabsContainer = document.querySelector("#tabs");
-
-        let tabTogglers = tabsContainer.querySelectorAll("#tabs a");
-
-        console.log(tabTogglers);
-
-        tabTogglers.forEach(function(toggler) {
-            toggler.addEventListener("click", function(e) {
-                e.preventDefault();
-
-                let tabName = this.getAttribute("href");
-
-                let tabContents = document.querySelector("#tab-contents");
-
-                for (let i = 0; i < tabContents.children.length; i++) {
-
-                    tabTogglers[i].parentElement.classList.remove("border-t", "border-r-2", "border-l-2",
-                        "-mb-px", "bg-gray-50");
-                    tabContents.children[i].classList.remove("hidden");
-                    if ("#" + tabContents.children[i].id === tabName) {
-                        continue;
-                    }
-                    tabContents.children[i].classList.add("hidden");
-
-                }
-                e.target.parentElement.classList.add("border-t", "border-r-2", "border-l-2", "-mb-px",
-                    "bg-gray-50");
-            });
-        });
-    </script>
 
     <script src="assets/vendor/flowbite/dist/flowbite.js"></script>
     <script src="assets/vendor/flowbite/dist/datepicker.js"></script>
---
+++ resources/views/livewire/card/caq-lead-report.blade.php
@@ -0,0 +1,4 @@
+<div class="bg-white border rounded-lg w-52 md:w-72 h-40 flex flex-col justify-center px-5">
+    <h1 class="text-blue-500 text-xl">CAQ Lead</h1>
+    <h1 class="text-blue-500 text-2xl font-semibold mt-2">Rp. 100.000.000</h1>
+</div>
