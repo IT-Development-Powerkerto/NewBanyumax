1e85a3448ef9025371d87750d4a10bb6c6e0dd29
HafidzDaffa <81839686+HafidzDaffa@users.noreply.github.com>
2022-07-07 10:40:28 +0700

    fix budgeting/routine eval adv

--- app/Http/Livewire/Table/BudgetingEvaluation.php
+++ app/Http/Livewire/Table/BudgetingEvaluation.php
@@ -3,11 +3,14 @@
 namespace App\Http\Livewire\Table;
 
 use Livewire\Component;
+use App\Models\Evaluation;
 
 class BudgetingEvaluation extends Component
 {
     public function render()
     {
-        return view('livewire.table.budgeting-evaluation');
+        $evaluations = Evaluation::all();
+        $data['jml_evaluation'] = Evaluation::all()->count();
+        return view('livewire.table.budgeting-evaluation',$data, compact('evaluations'));
     }
 }
