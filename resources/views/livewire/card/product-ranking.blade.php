3ef2007c473f3f5b6e847fe98caf4e0b21983e22
VEZA ADILOVIANA <18102143@ittelkom-pwt.ac.id>
2022-07-07 08:51:45 +0700

    add product ranking

---
+++ resources/views/livewire/card/product-ranking.blade.php
@@ -0,0 +1,99 @@
+<div class="bg-white p-5 rounded-md shadow-md">
+    <div class="font-bold text-md">
+        Product Rangking
+    </div>
+    <div class="overflow-x-auto mt-5">
+
+        <div class="min-w-max flex flex-row items-center gap-4 mb-5">
+            <div class="bg-clip-text text-transparent bg-gradient-to-r from-orange-500 to-red-500 font-semibold">
+                1
+            </div>
+            <div class="grid grid-cols-12 items-center w-full">
+                <div class="col-span-1">
+                    <img class="w-10 h-10 rounded-full shadow-xl" src="assets/img/team/team-1.png" alt="Ava image">
+                </div>
+                <div class="w-56 col-span-5 font-semibold text-sm text-gray-600">
+                    Etawaku Premium lor
+                </div>
+                <div class="w-20 col-span-2 font-semibold text-xs ">
+                    10 Leads
+                </div>
+                <div class="w-20 col-span-2 font-semibold text-xs ">
+                    8 Closing
+                </div>
+                <div class="w-20 col-span-2 font-semibold text-xs ">
+                    4.9 Juta
+                </div>
+            </div>
+        </div>
+
+        <div class="min-w-max flex flex-row items-center gap-4 mb-5">
+            <div class="bg-clip-text text-transparent bg-gradient-to-r from-orange-500 to-red-500 font-semibold">
+                2
+            </div>
+            <div class="grid grid-cols-12 items-center w-full">
+                <div class="col-span-1">
+                    <img class="w-10 h-10 rounded-full shadow-xl" src="assets/img/team/team-1.png" alt="Ava image">
+                </div>
+                <div class="w-56 col-span-5 font-semibold text-sm text-gray-600">
+                    Etawaku Premium lor
+                </div>
+                <div class="w-20 col-span-2 font-semibold text-xs ">
+                    10 Leads
+                </div>
+                <div class="w-20 col-span-2 font-semibold text-xs ">
+                    8 Closing
+                </div>
+                <div class="w-20 col-span-2 font-semibold text-xs ">
+                    4.9 Juta
+                </div>
+            </div>
+        </div>
+
+        <div class="min-w-max flex flex-row items-center gap-4 mb-5">
+            <div class="bg-clip-text text-transparent bg-gradient-to-r from-orange-500 to-red-500 font-semibold">
+                3
+            </div>
+            <div class="grid grid-cols-12 items-center w-full">
+                <div class="col-span-1">
+                    <img class="w-10 h-10 rounded-full shadow-xl" src="assets/img/team/team-1.png" alt="Ava image">
+                </div>
+                <div class="w-56 col-span-5 font-semibold text-sm text-gray-600">
+                    Etawaku Premium lor
+                </div>
+                <div class="w-20 col-span-2 font-semibold text-xs ">
+                    10 Leads
+                </div>
+                <div class="w-20 col-span-2 font-semibold text-xs ">
+                    8 Closing
+                </div>
+                <div class="w-20 col-span-2 font-semibold text-xs ">
+                    4.9 Juta
+                </div>
+            </div>
+        </div>
+
+        <div class="min-w-max flex flex-row items-center gap-4 mb-5">
+            <div class="bg-clip-text text-transparent bg-gradient-to-r from-orange-500 to-red-500 font-semibold">
+                4
+            </div>
+            <div class="grid grid-cols-12 items-center w-full">
+                <div class="col-span-1">
+                    <img class="w-10 h-10 rounded-full shadow-xl" src="assets/img/team/team-1.png" alt="Ava image">
+                </div>
+                <div class="w-56 col-span-5 font-semibold text-sm text-gray-600">
+                    Etawaku Premium lor
+                </div>
+                <div class="w-20 col-span-2 font-semibold text-xs ">
+                    10 Leads
+                </div>
+                <div class="w-20 col-span-2 font-semibold text-xs ">
+                    8 Closing
+                </div>
+                <div class="w-20 col-span-2 font-semibold text-xs ">
+                    4.9 Juta
+                </div>
+            </div>
+        </div>
+
+</div>
