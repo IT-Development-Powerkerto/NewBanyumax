4a4378adcaeefe4f3d2674345e860f0b9465bb14
VEZA ADILOVIANA <18102143@ittelkom-pwt.ac.id>
2022-07-07 11:39:04 +0700

    add tooltip for card caq lead n cust

--- resources/views/livewire/card/caq-customer-report.blade.php
+++ resources/views/livewire/card/caq-customer-report.blade.php
@@ -1,4 +1,8 @@
-<div class="bg-white border rounded-lg w-full md:w-72 h-40 flex flex-col justify-center px-5">
+<div data-tooltip-target="tooltip-caq-cust" type="button" class="cursor-pointer bg-white border rounded-lg w-full md:w-72 h-40 flex flex-col justify-center px-5">
     <h1 class="text-blue-500 text-xl">CAQ Customer</h1>
     <h1 class="text-blue-500 text-2xl font-semibold mt-2">Rp. 100.000.000</h1>
+</div>
+<div id="tooltip-caq-cust" role="tooltip" class="w-72 inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium bg-white text-blue-500 border rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip dark:bg-gray-700">
+    <div class="5"><div class="1">Lorem caq cust</div></div>
+    <div class="tooltip-arrow" data-popper-arrow></div>
 </div>
