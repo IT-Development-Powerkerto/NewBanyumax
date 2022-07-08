d4907f98bcbe3a8ef567db6fdfcbbc748f37b187
Muhammad Iqbal Ainu Rafie <74098172+ainurafie@users.noreply.github.com>
2022-07-08 14:43:49 +0700

    Halaman budgeting Adv di role adv

--- resources/views/livewire/table/data-budgeting-adv-log-activity.blade.php
+++ resources/views/livewire/table/data-budgeting-adv-log-activity.blade.php
@@ -3,10 +3,48 @@
         <div class="">
             <h1 class="font-semibold text-xl antialiased text-black tracking-wide">Data Budgeting ADV Log Activity</h1>
             <div class="flex flex-row gap-2 py-2">
-                <span class="font-medium text-sm text-zinc-400">1000 Data</span>
+                <span class="font-medium text-sm text-zinc-400">1.000 Data</span>
             </div>
         </div>
 
+
+        <div class="flex flex-col gap-2 md:gap-0 md:flex-row w-max">
+            <div class="relative mr-2">
+                <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                    <svg class="w-5 h-5 text-gray-400 dark:text-gray-400" fill="none" stroke="currentColor"
+                        viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                    </svg>
+                </div>
+                <input type="search" id="default-search"
+                    class="block px-4 py-2 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+                    placeholder="Search..">
+            </div>
+
+            <div class="relative mr-2 w-36 md:w-56">
+                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg class="w-5 h-5 text-gray-400 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M6 2a1 1 0 00-1 1v1H4a2 2 0  00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                </div>
+                <input name="daterange"
+                    class="bg-white text-gray-400 border-gray-200 shadow sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2 placeholder-gray-400 font-medium"
+                    placeholder="Select Date">
+            </div>
+            <div class="mr-2 w-36 md:w-fit">
+                <button
+                    class="text-gray-400 shadow w-full bg-white hover:bg-gradient-to-r from-cyan-500 to-blue-500 border hover:text-white font-medium rounded-lg text-sm px-4 py-2 text-center flex flex-row justify-center items-center gap-2"
+                    type="button" data-modal-toggle="add-budgeting-request-adv">
+                    <i class="las la-plus text-xl"></i>
+                    <span>Budgeting Request</span>
+                </button>
+            </div>
+            <livewire:modal.add-budgeting-request-adv />
+        </div>
     </div>
 </div>
 <div class="h-max bg-white overflow-x-auto">
@@ -153,7 +191,6 @@
         </ul>
     </nav>
 
-    <div
-    ></div>
+    <div></div>
 
 </div>
