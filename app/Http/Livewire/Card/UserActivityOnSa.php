71f9c33afdbe2ebe884d2adc34243dae14ff410c
VEZA ADILOVIANA <18102143@ittelkom-pwt.ac.id>
2022-07-13 14:20:42 +0700

    card php

---
+++ app/Http/Livewire/Card/UserActivityOnSa.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Card;
+
+use Livewire\Component;
+
+class UserActivityOnSa extends Component
+{
+    public function render()
+    {
+        return view('livewire.card.user-activity-on-sa');
+    }
+}
