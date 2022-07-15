19d46ee125674a6b56765cbbbcd5db442e8203f4
Muhammad Iqbal Ainu Rafie <74098172+ainurafie@users.noreply.github.com>
2022-07-15 21:44:03 +0700

    Add filter into table lead tunneling

---
+++ app/Http/Livewire/Modal/FilterLeadTunneling.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Modal;
+
+use Livewire\Component;
+
+class FilterLeadTunneling extends Component
+{
+    public function render()
+    {
+        return view('livewire.modal.filter-lead-tunneling');
+    }
+}
