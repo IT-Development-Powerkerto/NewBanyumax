7368a8dc2eb08d5900a15ab82c63eeb45f641a10
HafidzDaffa <81839686+HafidzDaffa@users.noreply.github.com>
2022-07-16 09:31:31 +0700

    Add budgeting request not fix

--- app/Http/Livewire/Modal/AddBudgetingRequestAdv.php
+++ app/Http/Livewire/Modal/AddBudgetingRequestAdv.php
@@ -3,11 +3,29 @@
 namespace App\Http\Livewire\Modal;
 
 use Livewire\Component;
+use App\Models\BudgetingDisbursement;
+use Carbon\Carbon;
 
 class AddBudgetingRequestAdv extends Component
 {
+    public $admin_id, $user_id, $role_id, $nominal, $no_rek, $target_omzet, $status;
+
     public function render()
     {
         return view('livewire.modal.add-budgeting-request-adv');
     }
+
+    public function store()
+    {
+        BudgetingDisbursement::create([
+            'admin_id'  => auth()->user()->admin_id,
+            'user_id'   => auth()->user()->id,
+            'campaign_id'   => $this->campaign_id,
+            'nominal'   => $this->nominal,
+            'no_rek'    => $this->no_rek,
+            'target_omzet'  => $this->target_omzet,
+            'created_at' => Carbon::now()->toDateTimeString(),
+        ]);
+    }
+
 }
--- resources/views/livewire/table/data-budgeting-adv-log-activity.blade.php
+++ resources/views/livewire/table/data-budgeting-adv-log-activity.blade.php
@@ -3,7 +3,7 @@
         <div class="">
             <h1 class="font-semibold text-xl antialiased text-black tracking-wide">Data Budgeting ADV Log Activity</h1>
             <div class="flex flex-row gap-2 py-2">
-                <span class="font-medium text-sm text-zinc-400">{{$jml_budgetdis}}</span>
+                <span class="font-medium text-sm text-zinc-400">{{$jml_budgetdis}} Data</span>
             </div>
         </div>
 
