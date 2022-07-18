cb47ad0920e2dc0bf629f94b08f0ab20c1366c36
HafidzDaffa <81839686+HafidzDaffa@users.noreply.github.com>
2022-07-18 13:55:52 +0700

    add budget adv not fix

--- app/Http/Livewire/Modal/AddBudgetingRequestAdv.php
+++ app/Http/Livewire/Modal/AddBudgetingRequestAdv.php
@@ -4,28 +4,47 @@ namespace App\Http\Livewire\Modal;
 
 use Livewire\Component;
 use App\Models\BudgetingDisbursement;
+use App\Models\Campaign;
+use App\Models\User;
 use Carbon\Carbon;
+use Livewire\WithFileUploads;
+
 
 class AddBudgetingRequestAdv extends Component
 {
+    use WithFileUploads;
     public $admin_id, $user_id, $role_id, $nominal, $no_rek, $target_omzet, $status;
+
+    protected $rules = [
+        'campaign_id'   =>'required',
+        'nominal'   => 'required',
+        'no_rek'    => 'required',
+        'target_omzet'  => 'required'
+    ];
+
+    // public function updated()
+    // {
+    //     $this->validate();
+    // }
+
+    public function store()
+    {
+        $advertiser = User::where('id', Campaign::where('id', $request->campaign_id)->value('user_id'))->value('name');
+
+        $validated = $this->validate();
+
+        $validated['admin_id'] = auth()->user()->admin_id;
+        $validated['user_id'] = auth()->user()->id;
+        $validated['advertiser'] = $advertiser;
+        $validated['created_at'] = Carbon::now()->toDateTimeString();
+
+        BudgetingDisbursement::create($validated);
+
+        $this->emit('disbursementCreated');
+    }
 
     public function render()
     {
         return view('livewire.modal.add-budgeting-request-adv');
     }
-
-    public function store()
-    {
-        BudgetingDisbursement::create([
-            'admin_id'  => auth()->user()->admin_id,
-            'user_id'   => auth()->user()->id,
-            'campaign_id'   => $this->campaign_id,
-            'nominal'   => $this->nominal,
-            'no_rek'    => $this->no_rek,
-            'target_omzet'  => $this->target_omzet,
-            'created_at' => Carbon::now()->toDateTimeString(),
-        ]);
-    }
-
 }
