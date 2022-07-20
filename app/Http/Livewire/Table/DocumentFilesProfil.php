311200640ba8543bb6616fd5593d9461754f878e
VEZA ADILOVIANA <18102143@ittelkom-pwt.ac.id>
2022-07-20 09:15:37 +0700

    create all table file on profil

---
+++ app/Http/Livewire/Table/DocumentFilesProfil.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Table;
+
+use Livewire\Component;
+
+class DocumentFilesProfil extends Component
+{
+    public function render()
+    {
+        return view('livewire.table.document-files-profil');
+    }
+}
