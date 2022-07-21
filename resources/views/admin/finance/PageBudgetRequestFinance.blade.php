0efce26b47c3be78c7fd1ed82fa20d4313d94f3b
Muhammad Iqbal Ainu Rafie <74098172+ainurafie@users.noreply.github.com>
2022-07-21 11:58:56 +0700

    Update Page Budgeting Request Admin

--- resources/views/admin/finance/PageBudgetRequestFinance.blade.php
+++ resources/views/admin/finance/PageBudgetRequestFinance.blade.php
@@ -136,21 +136,23 @@
 
 
     @livewireScripts
-    <script src="assets/vendor/flowbite/dist/flowbite.js"></script>
+    <script src="https://unpkg.com/flowbite@1.4.6/dist/flowbite.js"></script>
     <script src="assets/vendor/flowbite/dist/datepicker.js"></script>
+
+
     <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
     <script>
         $(function() {
-            $('input[name="selectdate"]').daterangepicker({
-                opens: 'left'
-            }, function(start, end, label) {
-                console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end
-                    .format('YYYY-MM-DD'));
-            });
+          $('input[name="daterange"]').daterangepicker({
+            opens: 'left'
+          }, function(start, end, label) {
+            console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+          });
         });
-    </script>
+        </script>
     <script>
         function previewImage() {
 
--- resources/views/livewire/table/budgetingreq-finance-admin.blade.php
+++ resources/views/livewire/table/budgetingreq-finance-admin.blade.php
@@ -16,6 +16,19 @@
                 </div>
                 <input type="search" id="default-search" class="block px-4 py-2 pl-10 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Search..">
             </div>
+            <div class="relative mr-2 w-36 md:w-56">
+                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                    <svg class="w-5 h-5 text-gray-400 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M6 2a1 1 0 00-1 1v1H4a2 2 0  00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                </div>
+                <input name="daterange"
+                    class="bg-white text-gray-400 border-gray-200 shadow sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2 placeholder-gray-400 font-medium"
+                    placeholder="Select Date">
+            </div>
             <div class="mr-2 w-36 md:w-fit">
                 <!-- Modal toggle -->
                 <button
@@ -74,7 +87,7 @@
             </div>
             <button
                 class="text-blue-400 h-max w-56 shadow bg-white hover:bg-gradient-to-r from-cyan-500 to-blue-500 border hover:text-white font-medium rounded-lg text-sm px-4 py-2 flex flex-row gap-2 items-center"
-                type="button" data-modal-toggle="request-budgeting-finance">
+                type="button" data-modal-toggle="add-budgetingreq-finance-admin">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd"
                         d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
@@ -82,6 +95,7 @@
                 </svg>
                 <span>Request Budgeting</span>
             </button>
+            <livewire:modal.add-budgetingreq-finance-admin />
         </div>
     </div>
 </div>
