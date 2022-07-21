508f8dd40603129409ce46302c612a6f0661b240
VEZA ADILOVIANA <18102143@ittelkom-pwt.ac.id>
2022-07-21 11:05:29 +0700

    page campaign - adv in role admin

--- resources/views/livewire/modal/add-operator.blade.php
+++ resources/views/livewire/modal/add-operator.blade.php
@@ -8,7 +8,7 @@
                     <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </button>
                 <div class="py-6 px-6 lg:px-8">
-                    <h3 class="mb-4 text-base font-semibold text-gray-900 dark:text-white border-b pb-2">Add Product</h3>
+                    <h3 class="mb-4 text-base font-semibold text-gray-900 dark:text-white border-b pb-2">Add Operator</h3>
                     <div class="relative">
                         <input type="text" class="w-full px-4 py-6 text-sm border border-black rounded-lg outline-none focus:bg-gray-400"
                           name="tags"
