5ff4e373d08249a175a47c00718608d3d87af5a9
VEZA ADILOVIANA <18102143@ittelkom-pwt.ac.id>
2022-07-22 10:31:36 +0700

    add list expedition, modal add expedition logistic

---
+++ app/Http/Livewire/Card/LogisticExpedition.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Card;
+
+use Livewire\Component;
+
+class LogisticExpedition extends Component
+{
+    public function render()
+    {
+        return view('livewire.card.logistic-expedition');
+    }
+}
