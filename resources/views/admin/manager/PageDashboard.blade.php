93586d798440910779dda668c1e1fc797871b99f
Muhammad Iqbal Ainu Rafie <74098172+ainurafie@users.noreply.github.com>
2022-07-22 10:01:56 +0700

    update dashboar manager

--- resources/views/admin/manager/PageDashboard.blade.php
+++ resources/views/admin/manager/PageDashboard.blade.php
@@ -21,7 +21,7 @@
 
 <body class="bg-white" style="font-family: 'Poppins', sans-serif;" x-data="sidebar()"
     @resize.window="handleResize()">
-    
+
     <div class="flex">
         <livewire:sidebar.sidebar-admin />
         <main class="w-screen">
@@ -103,46 +103,42 @@
 
                 </div>
             </div>  
-
-            <div class="px-5">
-                <div class="flex flex-row justify-between items-center py-3">
-                    <h1 class="text-black font-semibold tracking-wide text-xl" style="color: #7E8299">Dashboard Information
-                    </h1>
-                    <button id="FilterReporting" data-dropdown-toggle="FiltersReporting"
-                        class="text-blue-400 shadow bg-white hover:bg-gradient-to-r from-cyan-500 to-blue-500 border hover:text-white font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center"
-                        type="button">Daily <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
-                        </svg></button>
-                    <!-- Dropdown menu -->
-                    <div id="FiltersReporting" class="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow-lg w-32">
-                        <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="FilterReporting">
-                            <li>
-                                <a href="#" class="block px-4 py-2 hover:bg-gray-100 hover:text-blue-700">Daily</a>
-                            </li>
-                            <li>
-                                <a href="#" class="block px-4 py-2 hover:bg-gray-100 hover:text-blue-700">Weekly</a>
-                            </li>
-                            <li>
-                                <a href="#" class="block px-4 py-2 hover:bg-gray-100 hover:text-blue-700">Monthly</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
         
             <div class="flex flex-wrap justify-between pb-5 gap-2 mx-5">
-                <livewire:card.warehouse-admin />
-                <livewire:card.payment-admin />
-                <livewire:card.staff-budgeting-request-admin />
+                <livewire:card.leads />
+                <livewire:card.omzet />
+                <livewire:card.expense />
+            </div>
+        
+            <div class="items-center py-3 px-5">
+                <h1 class="text-black font-semibold tracking-wide text-xl" style="color: #7E8299">Dashboard Information
+                </h1>
+            </div>
+            <div class="w-full p-5">
+                <div class="flex flex-col md:grid lg:grid sm:grid grid-cols-3 gap-4 ">
+                    <livewire:card.advertising-cost-admin />
+                    <livewire:card.total-lead-admin />
+                    <livewire:card.total-closing-admin />
+                </div>
+                <div class="flex flex-col md:grid lg:grid sm:grid grid-cols-4 gap-4 py-5">
+                    <livewire:card.total-box-admin />
+                    <livewire:card.cost-per-result-admin />
+                    <livewire:card.cross-selling-admin />
+                    <livewire:card.caq-lead-admin />
+                </div>
             </div>
             <div class="mt-5 mx-5 px-6 py-2 rounded-md bg-gradient-to-r from-orange-500 to-red-600 text-white font-bold">
-                Data Budgeting
+                Information
             </div>
         
             <div class="container mx-auto px-5 mb-10">
-                <livewire:table.customer-service />
-            <livewire:table.data-closing />
+                <livewire:table.weekly-info-adv-admin />
+                <livewire:table.budgeting-request-adv-admin />
+                <livewire:table.budgeting-request-non-adv-admin />
+                <livewire:table.realization-adv2 />
+                <livewire:table.realization-non-adv />
+                <livewire:table.activity-evaluation />
+                <livewire:table.activity-logs-ceo />
             </div>
 
         </main>
