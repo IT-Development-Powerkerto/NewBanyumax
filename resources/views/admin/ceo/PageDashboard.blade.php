a74ec1e0898763b466a47e4da18a2d6dcbca739c
Muhammad Iqbal Ainu Rafie <74098172+ainurafie@users.noreply.github.com>
2022-07-22 10:01:34 +0700

    update dashboard ceo

--- resources/views/admin/ceo/PageDashboard.blade.php
+++ resources/views/admin/ceo/PageDashboard.blade.php
@@ -103,33 +103,6 @@
 
                 </div>
             </div>  
-
-            <div class="px-5">
-                <div class="flex flex-row justify-between items-center py-3">
-                    <h1 class="text-black font-semibold tracking-wide text-xl" style="color: #7E8299">Dashboard Information
-                    </h1>
-                    <button id="FilterReporting" data-dropdown-toggle="FiltersReporting"
-                        class="text-blue-400 shadow bg-white hover:bg-gradient-to-r from-cyan-500 to-blue-500 border hover:text-white font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center"
-                        type="button">Daily <svg class="w-4 h-4 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                            xmlns="http://www.w3.org/2000/svg">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
-                        </svg></button>
-                    <!-- Dropdown menu -->
-                    <div id="FiltersReporting" class="z-10 hidden bg-white divide-y divide-gray-100 rounded shadow-lg w-32">
-                        <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="FilterReporting">
-                            <li>
-                                <a href="#" class="block px-4 py-2 hover:bg-gray-100 hover:text-blue-700">Daily</a>
-                            </li>
-                            <li>
-                                <a href="#" class="block px-4 py-2 hover:bg-gray-100 hover:text-blue-700">Weekly</a>
-                            </li>
-                            <li>
-                                <a href="#" class="block px-4 py-2 hover:bg-gray-100 hover:text-blue-700">Monthly</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
         
             <div class="flex flex-wrap justify-between pb-5 gap-2 mx-5">
                 <livewire:card.leads />
