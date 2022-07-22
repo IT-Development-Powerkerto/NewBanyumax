83ec7a0d55b0807bbf2cd272ac7d46286c0f9609
Muhammad Iqbal Ainu Rafie <74098172+ainurafie@users.noreply.github.com>
2022-07-22 10:00:51 +0700

    dashboard inputer admin

---
+++ app/Http/Livewire/Modal/AddCs.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Modal;
+
+use Livewire\Component;
+
+class AddCs extends Component
+{
+    public function render()
+    {
+        return view('livewire.modal.add-cs');
+    }
+}
