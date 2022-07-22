7a1c651533a15db22006861e65928eaf42f50283
VEZA ADILOVIANA <18102143@ittelkom-pwt.ac.id>
2022-07-22 15:14:31 +0700

    tabs info prod, card: omzet, bottle, lead,closing

---
+++ app/Http/Livewire/Table/DashboardInformationProduct.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Table;
+
+use Livewire\Component;
+
+class DashboardInformationProduct extends Component
+{
+    public function render()
+    {
+        return view('livewire.table.dashboard-information-product');
+    }
+}
---
+++ resources/views/livewire/card/card-lead.blade.php
@@ -0,0 +1,11 @@
+<div class="bg-white border rounded-lg shadow-lg p-3" style="width: 13rem">
+    <div class="flex flex-row items-center gap-4">
+        <div>
+            <img src="../../assets/img/login.png" class="w-14 h-14 border shadow-md" alt="">
+            <p class="text-xs font-semibold py-2 text-gray-600">Generous</p>
+        </div>
+        <div class="text-2xl tracking-wide font-bold text-blue-600">
+            18 Lead
+        </div>
+    </div>
+</div>
