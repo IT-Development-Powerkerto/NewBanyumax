acb9f667c03b6c995b901cc734a84056dd076b05
HafidzDaffa <81839686+HafidzDaffa@users.noreply.github.com>
2022-07-22 15:49:13 +0700

    Add reimbursement and table

--- app/Http/Livewire/Table/Reimbursment.php
+++ app/Http/Livewire/Table/Reimbursment.php
@@ -7,8 +7,12 @@ use App\Models\Reimbursement as Reimburs;
 
 class Reimbursment extends Component
 {
+    public $listeners = [
+        'reimbursementCreated' => '$refresh'
+    ];
     public function render()
     {
+
         $reimbursements = Reimburs::all();
         $data['jml_reimbursment'] = Reimburs::all()->count();
         return view('livewire.table.reimbursment', $data, compact('reimbursements'));
