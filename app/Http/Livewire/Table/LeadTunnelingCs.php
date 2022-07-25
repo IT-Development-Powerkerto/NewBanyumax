cf0a67bd16a48026425d926e095242f439f465f4
Muhammad Iqbal Ainu Rafie <74098172+ainurafie@users.noreply.github.com>
2022-07-25 14:46:53 +0700

    lead-tunneling-cs

---
+++ app/Http/Livewire/Table/LeadTunnelingCs.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Table;
+
+use Livewire\Component;
+
+class LeadTunnelingCs extends Component
+{
+    public function render()
+    {
+        return view('livewire.table.lead-tunneling-cs');
+    }
+}
