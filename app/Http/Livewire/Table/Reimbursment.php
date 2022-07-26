ae99d573518fcec4a2f81b94b72bdd54cd7200ab
HafidzDaffa <81839686+HafidzDaffa@users.noreply.github.com>
2022-07-26 09:57:57 +0700

    Reimbursment fix

--- app/Http/Livewire/Table/Reimbursment.php
+++ app/Http/Livewire/Table/Reimbursment.php
@@ -19,18 +19,16 @@ class Reimbursment extends Component
     public function render()
     {
 
-        // $reimbursements = $this->search == null ?
-        //     Reimburs::with(['user'])->latest()->paginate($this->paginate):
-        //     Reimburs::where('id', 'like', '%'.$this->search.'%')
-        //     ->orWhere('reason', 'like', '%'.$this->search.'%')
-        //     ->orWhere('phone', 'like', '%'.$this->search.'%')
-        //     // ->orWhere('nominal', 'like', '%'.$this->search.'%')
-        //     ->orWhere('no_rekening', 'like', '%'.$this->search.'%')
-        //     ->orWhereHas('user', function($q){
-        //         $q->where('name', 'like', '%'.$this->search.'%');
-        //     })->latest()->paginate($this->paginate);
-
-        $reimbursements = Reimburs::all();
+        $reimbursements = $this->search == null ?
+            Reimburs::with(['user'])->latest()->paginate($this->paginate):
+            Reimburs::where('id', 'like', '%'.$this->search.'%')
+            ->orWhere('reason', 'like', '%'.$this->search.'%')
+            ->orWhere('phone', 'like', '%'.$this->search.'%')
+            // ->orWhere('nominal', 'like', '%'.$this->search.'%')
+            ->orWhere('no_rekening', 'like', '%'.$this->search.'%')
+            ->orWhereHas('user', function($q){
+                $q->where('name', 'like', '%'.$this->search.'%');
+            })->latest()->paginate($this->paginate);
 
         $data['jml_reimbursment'] = Reimburs::all()->count();
         return view('livewire.table.reimbursment', $data, compact('reimbursements'));
