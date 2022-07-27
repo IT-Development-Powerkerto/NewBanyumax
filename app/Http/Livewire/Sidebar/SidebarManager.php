db03ba56bf3f0695db9a660e394f986b8772acb0
Muhammad Iqbal Ainu Rafie <74098172+ainurafie@users.noreply.github.com>
2022-07-27 08:47:49 +0700

    sidebar manager. sidebar cs

---
+++ app/Http/Livewire/Sidebar/SidebarManager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Sidebar;
+
+use Livewire\Component;
+
+class SidebarManager extends Component
+{
+    public function render()
+    {
+        return view('livewire.sidebar.sidebar-manager');
+    }
+}
